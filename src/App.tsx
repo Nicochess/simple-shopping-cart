a14337494a6bb196c2176490660745651ea86174
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-07-01 13:22:26 -0300

    feat: snackbar feedback added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import { useState } from "react";
 import { useQuery } from "react-query";
-import { Drawer, CircularProgress, Badge, Box } from "@mui/material";
+import { Drawer, CircularProgress, Badge, Box, Snackbar } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
 import { Wrapper, StyledButton } from "./App.styles";
 import Item from "./components/Item/Item";
@@ -16,6 +16,7 @@ const getProducts = async (): Promise<CartItemType[]> => {
 const App: React.FC = () => {
   const [cartOpen, setCartOpen] = useState<boolean>(false);
   const [state, dispatch] = useReducer(cartReducer, []);
+  const [show, setShow] = useState<boolean>(false);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "Products",
     getProducts
@@ -28,10 +29,19 @@ const App: React.FC = () => {
 
   const addToCart = (item: CartItemType) => {
     dispatch({ type: ActionKind.addToCart, payload: item });
+    setShow(true)
   };
 
   const handleRemoveFromCart = (id: number) => {
-    dispatch({ type: ActionKind.removeToCart, payload: id })
+    dispatch({ type: ActionKind.removeToCart, payload: id });
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setShow(false);
   };
 
   if (isLoading) return <CircularProgress />;
@@ -46,6 +56,13 @@ const App: React.FC = () => {
           removeFromCart={handleRemoveFromCart}
         />
       </Drawer>
+
+      <Snackbar
+        message="Product added to cart"
+        open={show}
+        autoHideDuration={1500}
+        onClose={handleClose}
+      />
 
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(state)} color="error">
